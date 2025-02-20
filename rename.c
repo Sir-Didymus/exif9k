509fc5fdc839283be56e4dc736442bfea439dba7
Felix Osterbeck <cs-patzer@protonmail.com>
2025-02-20 16:10:02 +0100

    Start working on rename function

---
+++ rename.c
@@ -0,0 +1,6 @@
+
+#include "rename.h"
+
+int rename_photos(const char* dir_path) {
+    return 0;
+}
---
+++ rename.h
@@ -0,0 +1,8 @@
+
+#ifndef RENAME_H
+#define RENAME_H
+
+int rename_photos(const char* dir_path); 
+
+#endif
+
